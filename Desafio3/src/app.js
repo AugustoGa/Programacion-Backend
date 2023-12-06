591723cc7d9bf828a9cffbb4bdc8d0411fc126f2
AugustoGa <Augustogallo98@gmail.com>
2023-12-06 20:48:05 -0300

    Cambios en scripts ¬.¬

--- Desafio3/src/app.js
+++ Desafio3/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ProductManager = require('./Product.js')
 
 
 const app = express();
@@ -6,8 +7,34 @@ const port = 3000;
 
 const productManager = new ProductManager('product.json');
 
-app.get('/users',(req, rest)=>{
+app.get('/product', async (req, res)=>{
+    try{
+        await productManager.loadProducts();
 
+        const limit = req.query.limit;
+        const products = limit ? productManager.getProducts().splice(0, limit) : productManager.getProducts();
+        res.status(200).json(products);
+    }catch(error){
+        res.status(500).json({error: ' Internal Server Error'});
+    }
+
+});
+
+app.get('/products/:pid',async(req, res)=>{
+    try{
+        await productManager.loadProducts();
+
+        const productId = parseInt(req.params.pid);
+        const product = productManager.getProductById(productId);
+
+        if(product){
+            res.json(product);
+        }else{
+            res.status(404).json({error: 'Product not found'});
+        }
+    }catch(error){
+        res.status(500).json({error: ' Internal Server Error'});
+    }
 });
 
 
