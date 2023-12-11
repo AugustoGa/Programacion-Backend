c03ae367d20ed7bbb096a89e502a232d807ee927
AugustoGa <Augustogallo98@gmail.com>
2023-12-10 23:54:49 -0300

    entrega

--- Desafio3/src/app.js
+++ Desafio3/src/app.js
@@ -5,14 +5,15 @@ const ProductManager = require('./Product.js')
 const app = express();
 const port = 3000;
 
-const productManager = new ProductManager('product.json');
+const productManager = new ProductManager('productos.json');
 
-app.get('/product', async (req, res)=>{
+app.get('/products', async (req, res)=>{
     try{
         await productManager.loadProducts();
-
         const limit = req.query.limit;
-        const products = limit ? productManager.getProducts().splice(0, limit) : productManager.getProducts();
+        const products = limit 
+        ? productManager.getProducts().splice(0, limit)
+         : productManager.getProducts();
         res.status(200).json(products);
     }catch(error){
         res.status(500).json({error: ' Internal Server Error'});
